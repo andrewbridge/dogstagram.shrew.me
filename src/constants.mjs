82a2a3435deaa8044e3fd77a3f2c9018e7215b0c
Andrew Bridge <andrew@andrewhbridge.co.uk>
2025-03-08 17:36:34 +0000

    chore: add constants file

---
+++ src/constants.mjs
@@ -0,0 +1,11 @@
+export const DOG_VARIANTS = ['brown', 'black', 'white', 'black-white', 'brown-white'];
+export const DOG_STATES = [
+    'crouched',
+    'eating',
+    'jumping',
+    'lying',
+    'running',
+    'sitting',
+    'sniffing',
+    'walking'
+];
